8af93dc1c64cc05d87ac535fb6ee48d0d5e3ec62
Marcus Lewis <mrcslws@gmail.com>
2015-10-30 11:42:54 -0700

    Run/pause button now toggles on and off, but doesn't do anything

--- simulation.py
+++ simulation.py
@@ -5,19 +5,37 @@ class Simulation(object):
         self.model = model
         self.journal = journal
         self.inputIter = inputIter
+        self.statusSubscribers = []
+        self.isGoing = False
+
+    def pushStatusToSubscribers(self):
+        for subscriber in self.statusSubscribers:
+            subscriber.put([self.isGoing])
 
     def handleMessage(self, msg):
         command = str(msg[0])
+        args = msg[1:]
         if command == "connect":
-            print "connect!"
+            pass
+        elif command == "run":
+            self.isGoing = True
+            self.pushStatusToSubscribers()
+        elif command == "pause":
+            self.isGoing = False
+            self.pushStatusToSubscribers()
+        elif command == "toggle":
+            self.isGoing = not self.isGoing
+            self.pushStatusToSubscribers()
         elif command == "step":
             timestampStr, consumptionStr = self.inputIter.next()
             self.model.run({
                 "timestamp": datetime.datetime.strptime(timestampStr, "%m/%d/%y %H:%M"),
                 "kw_energy_consumption": float(consumptionStr),
             })
-
             self.journal.append(self.model)
-
+        elif command == "subscribe-to-status":
+            subscriberChannel, = args
+            self.statusSubscribers.append(subscriberChannel)
+            subscriberChannel.put(self.isGoing)
         else:
             print "Unrecognized command! %s" % command
